1f9810cb2ca7637bf0bf413b945933b57bc94183
Nathan Atchison <Natchison25@my.whitworth.edu>
2025-10-23 15:57:35 -0700

    Working on LAN Discovery and TCP Socket connection

---
+++ src/lan_discovery.ts
@@ -0,0 +1,38 @@
+import { BrowserWindow } from "electron";
+import dgram from "dgram"
+
+const DISCOVERY_PORT = 5000;
+const DISCOVERY_MSG = "LAN_CHAT_DISCOVERY";
+
+export const startLANDiscovery = (win: BrowserWindow) => {
+    // UDP socket used for discovering other users
+    const discoverySocket = dgram.createSocket("udp4");
+    
+    discoverySocket.on("message", (msg, rinfo) => {
+      if (msg.toString() === DISCOVERY_MSG) {
+        console.log("Found user:", rinfo.address);
+        // Tell the other user that we exist
+        discoverySocket.send(Buffer.from("LAN_CHAT_RESPONSE"), rinfo.port, rinfo.address);
+        // Notify Front-End that we have found another user to possibly chat with
+        win.webContents.send("user-found", { ip: rinfo.address })
+      }
+    })
+    
+    discoverySocket.bind(DISCOVERY_PORT, () => {
+      discoverySocket.setBroadcast(true);
+      console.log(`Listening for LAN peers on port ${DISCOVERY_PORT}`)
+    });
+    
+    // Broadcast our presence every 5 seconds
+    setInterval(() => {
+      discoverySocket.send(
+        Buffer.from(DISCOVERY_MSG),
+        0,
+        DISCOVERY_MSG.length,
+        DISCOVERY_PORT,
+        "255.255.255.255"
+      );
+    }, 5000)
+
+    return discoverySocket;
+}
---
+++ src/tcp_chat_server.ts
@@ -0,0 +1,47 @@
+import { BrowserWindow } from "electron";
+import net from "net"
+
+const TCP_PORT = 5001;
+
+export const startTCPServer = (win: BrowserWindow) => {
+    const clients: net.Socket[] = []
+    
+    const tcpServer = net.createServer((socket) => {
+        console.log("New client connected:", socket.remoteAddress);
+        clients.push(socket);
+    
+        // Listen for data from the clients
+        socket.on("data", (data) => {
+            const message = data.toString();
+    
+            clients.forEach((s) => {
+                if(s !== socket) s.write(message);
+            });
+    
+            win.webContents.send("chat-message", {text: message});
+        });
+    
+        socket.on("close", () => {
+            console.log("Client disconnected:", socket.remoteAddress);
+            const disconnect_index = clients.indexOf(socket);
+            if(disconnect_index >= 0) clients.splice(disconnect_index, 1);
+        });
+    });
+    
+    tcpServer.listen(TCP_PORT, () => {
+        console.log(`TCP Server listnening on port ${TCP_PORT}`)
+    })
+    
+    const connectToPeer = (ip: string) => {
+        const client = net.createConnection({ host: ip, port: TCP_PORT }, () => {
+            console.log("Connect to:", ip);
+        });
+    
+        client.on("data", (data) => {
+            const message = data.toString();
+            win.webContents.send("chat-message", {text: message});
+        })
+    
+        client.on("end", () => console.log("Disconnected from:", ip));
+    }
+}
